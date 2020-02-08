ec9e5a9c4401a74cf05e9542e195d376f9e14e59
Brian2524 <b2hinkle@gmail.com>
2020-02-07 23:24:40 -0500

    Changed the server page to be in a new controller called ServerController, not the HomeController

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -28,11 +28,6 @@ namespace WebApplication.Controllers
             return View();
         }
 
-        public IActionResult Servers()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
