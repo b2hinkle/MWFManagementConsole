bef0d09b86178747539e902801797a0be8e84f7a
Brian2524 <b2hinkle@gmail.com>
2020-04-18 13:57:27 -0400

    Add tabs in layout.cs to be how it should be, add controllers for each tab

---
+++ DeveloperConsole/WebApplication/Controllers/GameInstancesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class GameInstancesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
