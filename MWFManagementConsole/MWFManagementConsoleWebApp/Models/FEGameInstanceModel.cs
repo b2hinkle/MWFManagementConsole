61fb40a39f4b5b25df561d39839c2aa4fd4b92c7
Brian2524 <b2hinkle@gmail.com>
2021-01-22 00:17:03 -0500

    Add FEHostModle and FEGameInstanceModel
    Signed-off-by: Brian2524 <b2hinkle@gmail.com>

---
+++ MWFManagementConsole/MWFManagementConsoleWebApp/Models/FEGameInstanceModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MWFManagementConsoleWebApp.Models
+{
+    // Front end model for user forum data
+
+    // Users shouldn't be able to gie the GameInstanceModel values. They should only be able to create GameInstanceModels from an existing GameModel. 
+    // Might however give the option to override the values if they want slightly different values. But that is for a later time.
+    public class GameInstanceModel
+    {
+        //  This should be enum instead with dropdown for choosing
+        [Display(Name = "Game")]
+        [Required(ErrorMessage = "Required field")]
+        public string Game { get; set; }
+/*
+        [Display(Name = "Arguments")]
+        [Required(ErrorMessage = "Required field")]
+        public string Args { get; set; }
+*/
+        //  This should be enum instead with dropdown for choosing
+        [Display(Name = "Host to Launch On")]
+        [Required(ErrorMessage = "Required field")]
+        [StringLength(39, MinimumLength = 11, ErrorMessage = "Characters must be in the range 11-39")]
+        public string AssociatedHost { get; set; }
+    }
+}
