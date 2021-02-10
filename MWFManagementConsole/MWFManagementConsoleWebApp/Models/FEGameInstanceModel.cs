2b0dfe6d6178e659098086e621d49ffd21e676ab
Brian2524 <b2hinkle@gmail.com>
2021-02-10 02:10:29 -0500

    Switch from using "AssociatedHost" to "HostIp". Havn't decided how we will display the host ip at this point

--- MWFManagementConsole/MWFManagementConsoleWebApp/Models/FEGameInstanceModel.cs
+++ MWFManagementConsole/MWFManagementConsoleWebApp/Models/FEGameInstanceModel.cs
@@ -21,6 +21,6 @@ namespace MWFManagementConsoleWebApp.Models
         [Display(Name = "Host to Launch On")]
         [Required(ErrorMessage = "Required field")]
         [StringLength(39, MinimumLength = 11, ErrorMessage = "Characters must be in the range 11-39")]
-        public string AssociatedHost { get; set; }
+        public int HostId { get; set; }
     }
 }
